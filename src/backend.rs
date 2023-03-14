a619d05f95afcc5d6e814f9e174751c50bb97eae
Philipp Müller <phmueller2@hs-koblenz.de>
2023-03-14 21:53:43 +0100

    initial commit

--- src/backend.rs
+++ src/backend.rs
@@ -1,35 +1,164 @@
-use eframe::egui::{Options, Vec2};
+use std::os::raw::c_float;
+use std::sync::Arc;
+use eframe::egui;
+use eframe::egui::{Galley, Ui};
+use tracing_subscriber::fmt::format;
 
-struct state {
-    IOs : [IO; 2],
-    Name : &'static str,
-    ID : u8,
-    content :&'static str
-}
-struct IO{
-    IOVec : Vec<u8>,
-    Type : IO_type
-}
+#[derive(Debug, Clone, PartialEq)]
+pub(crate) struct State {
+    pub(crate) I : IO,
+    pub(crate) O : IO,
+    pub(crate) Name :  String,
+    pub(crate) ID : u8,
+    pub(crate) content : String,
+    pub(crate) frame : egui::epaint::RectShape,
+    pub(crate) IO_anker_template : egui::epaint::CircleShape,
 
-enum IO_type {
+}
+#[derive(Debug, Clone,PartialEq,)]
+pub(crate) struct IO{
+    pub(crate) IOVec : Vec<u8>,
+    pub(crate) Type : IoType
+}
+#[derive(Debug, Clone, PartialEq )]
+pub(crate) enum IoType {
     Input,
     Output
 }
-impl state {
-    fn default(n_In:usize,n_Out:usize,state_Name:String,Content:String)->Self{
-        let I : IO = IO{
-            IOVec : Vec::with_capacity(n_In),
-            Type : IO_type::Input,
-        };
-        let O : IO = IO{
-            IOVec : Vec::with_capacity(n_Out),
-            Type : IO_type::Output,
-        };
+impl State {
+
+    pub(crate) fn new(n_In:usize, n_Out:usize, state_Name:String, Content: String, state_ID:u8) ->Self{
+
         Self{
-            IOs : [I,O],
-            Name : state_Name.into(),
-            ID : u8,
-            content : Content.into()
+            O : IO{
+            IOVec : vec![0;n_Out],
+            Type : IoType::Output,
+            },
+            I : IO{
+            IOVec : vec![0;n_In],
+            Type : IoType::Input,
+            },
+            Name : state_Name,
+            ID : state_ID,
+            content : Content,
+            frame : egui::epaint::RectShape{
+                rect: egui::Rect{min: egui::Pos2 { x:40.0, y :40.0 },max: egui::Pos2{ x:100.0, y :200.0 }},
+                rounding: egui::Rounding{nw:0.2,ne:0.2,sw:0.2,se:0.2},
+                fill: egui::Color32::from_rgb(96, 96, 96),
+                stroke: egui::Stroke::NONE
+            },
+            IO_anker_template : egui::epaint::CircleShape{
+                center: egui::Pos2{x:44.0, y :46.0},
+                radius:3.0,
+                fill: egui::Color32::from_rgb(96, 96, 96),
+                stroke:egui::Stroke{width: 1.0,color: egui::Color32::from_rgb(220, 220, 220)
+                }},
+
+        }
+
+    }
+    pub(crate) fn Connect_IO(&mut self,Type : IoType, Pos : usize, State_ID : u8){
+        match Type{
+            IoType::Input => {
+                self.I.IOVec[Pos] = State_ID;
+            }
+            IoType::Output => {
+                self.O.IOVec[Pos] = State_ID;
+            }
+        }
+
+    }
+    pub(crate) fn change_IO_count(&mut self,Type : IoType,IOcount : usize){
+        match Type {
+            IoType::Input => {
+                self.I.IOVec = Vec::new();
+                self.I.IOVec = Vec::with_capacity(IOcount);
+                for i in 0..self.I.IOVec.len()-1 {
+                    self.I.IOVec.push(0);
+                }
+            },
+            IoType::Output => {
+                self.O.IOVec = Vec::new();
+                self.O.IOVec = Vec::with_capacity(IOcount);
+                for i in 0..self.O.IOVec.len()-1 {
+                    self.O.IOVec.push(0);
+                }
+            }
         }
     }
+    /// Output ungleich None, wenn ein IO gedrückt wurde: (IO-Typ,IO-Number State-ID)
+    pub(crate) fn Draw_IO(&mut self, ui: &mut egui::Ui) -> Option<clickedIO>{
+        let mut clicked_IO = None;
+        let mut leng_I : f32 = 0.0;
+        let mut leng_O : f32 = 0.0;
+        //==============Drawing Inputs==============
+        let mut offset:u8 = 0;
+        for i in  self.I.IOVec.iter_mut(){
+            let mut anker = self.IO_anker_template.clone();
+            if *i != 0 as u8 {
+                anker.fill = egui::Color32::from_rgb(255, 255, 255);
+            }
+            anker.center.y += offset as f32*10.0;
+            ui.painter().add(anker);
+            let r_a_i = ui.allocate_rect(anker.visual_bounding_rect(),egui::Sense::click_and_drag());
+            if r_a_i.clicked(){
+                *i = 2;
+                clicked_IO = Option::from(clickedIO{
+                    IOType : IoType::Input,
+                    IO_number : offset,
+                    State : self.ID,
+                });
+            }
+
+            offset += 1;
+            leng_I = offset as f32*10.0+6.0;
+        }
+        //==============Drawing Outputs==============
+        offset = 0;
+        for o in self.O.IOVec.iter_mut(){
+            let mut anker = self.IO_anker_template.clone();
+            if *o != 0 as u8 {
+                anker.fill = egui::Color32::from_rgb(255, 255, 255);
+            }
+            anker.center.x = self.frame.rect.max.x-4.0;
+            anker.center.y += offset as f32*10.0;
+            ui.painter().add(anker);
+            let r_a_o = ui.allocate_rect(anker.visual_bounding_rect(),egui::Sense::click_and_drag());
+            if r_a_o.clicked(){
+                *o = 2;
+                clicked_IO = Option::from(clickedIO{
+                    IOType : IoType::Output,
+                    IO_number : offset,
+                    State : self.ID,
+                });
+            }
+
+
+            offset += 1;
+            leng_O = offset as f32*10.0+6.0;
+        }
+        //==============Resize Rect==============
+        if self.frame.rect.max.y-self.frame.rect.min.y < leng_I {
+            self.frame.rect.max.y = leng_I;
+        }
+        else if  self.frame.rect.max.y-self.frame.rect.min.y < leng_O{
+            self.frame.rect.max.y = leng_O;
+        }
+        return clicked_IO
+    }
+    pub(crate) fn DrawTitle(&self, ui: &mut egui::Ui,ctx : &egui::Context){
+        let TitleRect : egui::Rect  = egui::Rect{
+            min : egui::Pos2{x:self.frame.rect.min.x+6.0, y:self.frame.rect.min.y+6.0},
+            max : egui::Pos2{x:self.frame.rect.max.x-6.0, y:self.frame.rect.min.y+26.0}
+        };
+        let mut TitleText : egui::widgets::Label = egui::widgets::Label::new(egui::RichText::from(self.Name.clone()).size(12.0));
+        ui.put(TitleRect,TitleText);
+
+    }
+}
+
+pub(crate) struct clickedIO{
+    pub(crate) IOType : IoType,
+    pub(crate) IO_number : u8,
+    pub(crate) State : u8,
 }
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,19 @@
 
 mod backend;
 
+use std::fmt::format;
 use eframe::egui;
-use eframe::egui::{Pos2, Response, Ui};
+use eframe::egui::{Pos2, Response, SidePanel, Ui};
 use eframe::egui::plot::PlotPoint;
 use eframe::epaint::RectShape;
+
 
 fn main() -> Result<(), eframe::Error> {
     // Log to stdout (if you run with `RUST_LOG=debug`).
     tracing_subscriber::fmt::init();
 
     let options = eframe::NativeOptions {
-        initial_window_size: Some(egui::vec2(320.0, 240.0)),
+        initial_window_size: Some(egui::vec2(1920.0, 1080.0)),
         ..Default::default()
     };
     eframe::run_native(
@@ -31,8 +33,13 @@ struct MyApp {
     square_name_vec : Vec<[char;128]>,
     square_rec_vec : Vec<egui::epaint::RectShape>,
     square_respone_vec : Vec<Response>,
-
-
+    state_vec : Vec<backend::State>,
+    sidebar_enabled : bool,
+    n_state : u8,
+    init_state : backend::State,
+    clicked_new_state : bool,
+    NewState : New_state_input,
+    clickedIO : Option<backend::clickedIO>,
 }
 
 impl Default for MyApp {
@@ -52,6 +59,32 @@ impl Default for MyApp {
             square_name_vec : vec![],
             square_rec_vec : vec![],
             square_respone_vec : vec![],
+            state_vec : vec![],
+            sidebar_enabled : false,
+            n_state : 0,
+            init_state : backend::State::new(5, 2, "".parse().unwrap(), "".parse().unwrap(), 0),/*backend::State {
+                I: backend::IO { Type: backend::IoType::Input, IOVec: Vec::with_capacity(0) },
+                O: backend::IO { Type: backend::IoType::Input, IOVec: Vec::with_capacity(0) },
+                Name : ("").parse().unwrap(),
+                ID : 0,
+                content : "".parse().unwrap(),
+                frame : egui::epaint::RectShape{
+                        rect: egui::Rect{min: egui::Pos2 { x:40.0, y :40.0 },max: egui::Pos2{ x:100.0, y :80.0 }},
+                        rounding: egui::Rounding{nw:0.2,ne:0.2,sw:0.2,se:0.2},
+                        fill: egui::Color32::from_rgb(255, 128, 128),
+                        stroke: egui::Stroke::NONE
+                        },
+
+            }*/
+            clicked_new_state : false,
+            NewState : New_state_input{
+                n_Input : 0,
+                n_Output : 0,
+                title : "".parse().unwrap(),
+                content : "".parse().unwrap(),
+
+            },
+            clickedIO : None,
         }
     }
 }
@@ -59,16 +92,33 @@ impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         self.frame.rect.min = self.square_pos;
         self.frame.rect.max = egui::Pos2 {x:self.square_pos.x+20.0,y:self.square_pos.y+20.0};
+        if self.clicked_new_state {
+            self.newState(ctx);
+
+        }
+        //====================Top Panel====================
         egui::TopBottomPanel::top("Testpanel").show(ctx, |ui|{
             ui.horizontal(|ui|{
                 if ui.button("Generate Square").clicked() {
-                    self.square_rec_vec.push(self.frame);
-                    self.square_respone_vec.push(ui.allocate_rect(egui::Rect{min: self.frame.rect.min,max: self.frame.rect.max},egui::Sense::drag()))
+                    self.n_state += 1;
+                    let mut state = self.init_state.clone();
+                    state.ID = self.n_state;
+                    //self.square_rec_vec.push(self.frame);
+                    self.state_vec.push(state);
+
+                }
+                if ui.button("Testbutton Window").clicked(){
+                    self.n_state += 1;
+                    let mut state = self.init_state.clone();
+                    self.clicked_new_state = true;
                 }
 
             });
         });
+        //====================Central Panel====================
+
         egui::CentralPanel::default().show(ctx, |ui| {
+            /*
             if self.click {
                 ui.heading("My egui Application");
             }
@@ -82,18 +132,27 @@ impl eframe::App for MyApp {
                 self.age += 1;
             }
             ui.label(format!("Hello '{}', age {}", self.name, self.age));
-            let mut delet_vec : Vec<RectShape> = Vec::new();
+            */
+
+            let mut delet_vec : Vec<backend::State> = Vec::new();
+            /*
             for i_frame in self.square_rec_vec.iter_mut(){
+                //===========State Painting===========
                 ui.painter().add(*i_frame);
                 let i_r = ui.allocate_rect(i_frame.rect,egui::Sense::drag());
+                //===========State Interaction===========
                 if i_r.clicked() {
-                    self.click = !self.click;
+                    self.sidebar_enabled = true;
                 }
                 if i_r.dragged(){
                     let delta = i_r.drag_delta();
                     i_frame.rect.min = Pos2{x: i_frame.rect.min.x+delta[0],y: i_frame.rect.min.y+delta[1]};
                     i_frame.rect.max = Pos2{x: i_frame.rect.max.x+delta[0],y: i_frame.rect.max.y+delta[1]};
-                }/*
+                }
+                if i_r.clicked_elsewhere(){
+                    self.sidebar_enabled = false;
+                }
+                /*
                 if i_r.secondary_clicked(){
                     delet_vec.push(*i_frame)
                 }*/
@@ -103,13 +162,71 @@ impl eframe::App for MyApp {
                         ui.close_menu();
                     }
                 });
-            }
+            };
+            */
+            let mut clicked_inframe : bool = false;
+            for i_state in self.state_vec.iter_mut(){
+                //===========State Painting===========
 
-            for i_frame in delet_vec{
-                self.square_rec_vec.retain(|&x| x!=i_frame);
-            }
-            ui.label(format!("Laenge des Vektors '{}'", self.square_rec_vec.len()));
+                ui.painter().add(i_state.frame);
+                i_state.DrawTitle(ui,ctx);
 
+                let i_r = ui.allocate_rect(i_state.frame.rect,egui::Sense::drag());
+                //===========State Interaction===========
+                if i_r.clicked() {
+                    self.sidebar_enabled = true;
+                    clicked_inframe = true;
+                }
+                if i_r.dragged(){
+                    let delta = i_r.drag_delta();
+                    i_state.frame.rect.min = Pos2{x: i_state.frame.rect.min.x+delta[0],y: i_state.frame.rect.min.y+delta[1]};
+                    i_state.frame.rect.max = Pos2{x: i_state.frame.rect.max.x+delta[0],y: i_state.frame.rect.max.y+delta[1]};
+                    i_state.IO_anker_template.center = Pos2{x: i_state.IO_anker_template.center.x+delta[0], y: i_state.IO_anker_template.center.y+delta[1]}
+                }
+                if i_r.clicked_elsewhere() & !clicked_inframe{
+                    self.sidebar_enabled = false;
+                }
+                /*
+                if i_r.secondary_clicked(){
+                    delet_vec.push(*i_frame)
+                }*/
+                i_r.context_menu(|ui|{
+                    if ui.button("Delete").clicked(){
+                        delet_vec.push(i_state.clone());
+                        ui.close_menu();
+                    };
+                    ui.text_edit_singleline(&mut self.name);
+                });
+                self.clickedIO = i_state.Draw_IO(ui);
+            };
+            for i_state in delet_vec {
+                self.state_vec.retain(|x| x != &i_state);
+            }
+            match &self.clickedIO {
+                None => {;},
+                Some(backend::clickedIO {IOType,IO_number,State}) =>{
+                    for state in self.state_vec.iter_mut(){
+                        if state.ID == self.clickedIO.as_ref().unwrap().State{
+                            match ctx.pointer_hover_pos(){
+                                None => {;},
+                                Some(egui::Pos2{x,y}) => {
+                                    match self.clickedIO.as_ref().unwrap().IOType {
+                                        backend::IoType::Input => {
+                                            let Line = egui::epaint::Shape::LineSegment {points: [ctx.pointer_hover_pos().unwrap().clone(),egui::Pos2{x: state.IO_anker_template.center.x,y: state.IO_anker_template.center.y + self.clickedIO.as_ref().unwrap().IO_number.clone() as f32*10.0+6.0}],
+                                                stroke : egui::Stroke{width : 2.0, color : egui::Color32::default()}
+                                            };
+                                            ui.painter().add(Line);
+                                        },
+                                        backend::IoType::Output =>{;}
+                                    }
+
+                                }
+                            }
+
+                        }
+                    }
+                }
+            }
             /* Working Code
             ui.painter().add(self.frame);
             let r = ui.allocate_rect(egui::Rect{min: self.frame.rect.min,max: self.frame.rect.max},egui::Sense::drag());
@@ -131,13 +248,59 @@ impl eframe::App for MyApp {
             )*/
         });
 
+        //====================Right Sidepanel====================
+        if self.sidebar_enabled{
+            egui::SidePanel::right("Propotries").show(ctx,|ui|{
+                if ui.button("Test").clicked(){
+                    ;
+                }
+            });
+        }
+
     }
 }
 
 impl MyApp {
-    fn rightclickMenu(&mut self, ui: &mut egui::Ui,frame: egui::epaint::RectShape) {
-        if ui.button("Test").clicked(){
-            ui.close_menu();
-        }
+    fn newState(&mut self, ctx : &egui::Context) {
+        egui::Window::new("Parameter für den neuen Zustand").show(ctx, |ui| {
+            ui.vertical(|ui| {
+                ui.label(format!("Zustands-ID: {}",self.n_state));
+                ui.horizontal(|ui|{
+                    ui.label("Zustandsname: ");
+                    ui.text_edit_singleline(&mut self.NewState.title);
+                });
+                ui.add(egui::Slider::new(&mut self.NewState.n_Input, 0..=10).text("Anzahl der Inputs"));
+                ui.add(egui::Slider::new(&mut self.NewState.n_Output, 0..=10).text("Anzahl der Outputs"));
+                ui.label("Zustandsinhalt: ");
+                ui.text_edit_multiline(&mut self.NewState.content).on_hover_text("Hier durchzuführende Funktionen angeben");
+                ui.horizontal(|ui|{
+                    if ui.button("Close window").clicked(){
+                        self.clicked_new_state =false;
+                        self.n_state -=1;
+                    };
+                    if ui.button("Create State").clicked(){
+                        let state : backend::State = backend::State::new(self.NewState.n_Input,self.NewState.n_Output,self.NewState.title.clone(),self.NewState.content.clone(),self.n_state);
+                        self.state_vec.push(state);
+                        self.NewState = New_state_input{
+                            n_Input : 0,
+                            n_Output : 0,
+                            title : "".parse().unwrap(),
+                            content : "".parse().unwrap(),
+                        };
+                        self.clicked_new_state =false;
+                    };
+
+                });
+            });
+
+
+        });
     }
+}
+
+struct New_state_input{
+    n_Input : usize,
+    n_Output : usize,
+    title : String,
+    content : String,
 }
