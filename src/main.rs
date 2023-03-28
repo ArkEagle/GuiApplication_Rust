280da162db94af6a66e8efa059b5eb75e1206f69
Philipp Mueller <runes.arkan@gmail.com>
2023-03-28 13:23:14 +0200

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-
+//TODO: eventuell Sprungmarken hinzufügen
+//TODO: Systematische Simulation (Recherche: zB Sackgassen)
 mod backend;
 
 use backend::{clickedIO, State};
